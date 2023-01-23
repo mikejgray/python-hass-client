9fa387cc166117f79a60cade8fe931fe9633b432
Mike <mike@graywind.org>
2023-01-22 19:54:27 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Topic :: Home Automation",
